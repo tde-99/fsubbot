fbe49764fd04789c46d1398a0e26d61359ca99f9
tde-99 <deltaentertainmentofc@gmail.com>
2025-07-15 23:10:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
-# bot.py placeholder
+# bot.py
+
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+Bot = Client(
+    "ForceSubReferralBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="bot")
+)
